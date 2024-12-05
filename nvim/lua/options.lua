b9bd397d5badbe0eb4bf4ebe3255cc0f93a4c8fa
SergioPerera <alu0101394503@ull.edu.es>
2024-12-05 19:39:48 +0000

    as

---
+++ nvim/lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+-- Mostrar números absolutos en la línea actual y números relativos en otras líneas
+vim.wo.number = true          -- Habilita los números de línea
+vim.wo.relativenumber = true  -- Habilita los números relativos
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
